b4dc393699e1825750ae42f2a5da3a5a3d37dcf4
Benjamin-Bailes <139053033+Benjamin-Bailes@users.noreply.github.com>
2023-09-19 16:06:58 +0930

    oiefhoejhioew

--- Entity.h
+++ Entity.h
@@ -1,7 +1,19 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
 class Entity {
  protected:
-  int xPosition;
-  int yPosition;
+  int xPos;
+  int yPos;
 
  public:
+  Entity();
+  Entity(int _xPos, int _yPos);
+
+  ~Entity();
+
+  virtual int getPosX() = 0;
+  virtual int getPosY() = 0;
 };
+
+#endif  // ENTITY
