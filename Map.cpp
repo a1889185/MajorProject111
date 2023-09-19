c17bab9e09e91a0f5e69c1102044d967e8b0e066
Benjamin-Bailes <139053033+Benjamin-Bailes@users.noreply.github.com>
2023-09-19 18:59:17 +0930

    misc

---
+++ Map.cpp
@@ -0,0 +1,14 @@
+#include "Map.h"
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+#include "NonMoveableEntity.h"
+
+Map::Map() {}
+
+// generate matrix of 1's - walls, and 0's - paths
+int[42][42] Map::generateMap() {}
+
+//  set allWalls  with positions based on matrix of walls
+void Map::setWallPostions(int[64][64] _matrixOfWalls) {}
--- Map.h
+++ Map.h
@@ -2,12 +2,29 @@
 #define MAP_H
 
 #include <SFML/Graphics.hpp>
+#include <string>
+
+#include "NonMoveableEntity.h"
 
 class Map {
  protected:
+  //  matrix of 1's - walls, and 0's - paths
   int** matrixOfWalls;
 
+  int numWalls;
+  NonMoveableEntity** allWalls;
+
+  int id;
+  std::string name;
+
  public:
+  Map();
+
+  // generate matrix of 1's - walls, and 0's - paths
+  int[42][42] generateMap();
+
+  //  set allWalls  with positions based on matrix of walls
+  void setWallPostions(int[64][64] _matrixOfWalls);
 };
 
 #endif  // MAP_H
