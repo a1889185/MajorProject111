40b85a15357bf37ede77be4ad4f9ade2fe77c32e
Benjamin-Bailes <139053033+Benjamin-Bailes@users.noreply.github.com>
2023-09-20 14:35:46 +0930

    misc

--- Map.cpp
+++ Map.cpp
@@ -6,10 +6,24 @@
 #include "NonMoveableEntity.h"
 
 Map::Map() {}
+Map::~Map() {}
 
 // generate matrix of 1's - walls, and 0's - paths
 int[42][42] Map::generateMap() {}
 
-//  set allWalls  with positions based on matrix of walls
-void Map::setWalls(*int[64][64] _matrixOfWalls) {}
-NonMoveableEntity** Map::getWalls() {}
+void Map::setNumWalls(*int[42][42] mapMatrix) {
+  int i, j;
+  for (i = 0; i < 42; i++) {
+    for (j = 0; j < 42; j++) {
+      if (mapMatrix[i][j] == 1) {
+        this->numWalls++;
+      }
+    }
+  }
+}
+
+// set allWalls  with positions based on matrix of walls
+void Map::setWalls(*int[64][64] mapMatrix) {}
+NonMoveableEntity** Map::getWalls() { return this->allWalls; }
+
+void mapDisplay();
--- Map.h
+++ Map.h
@@ -9,7 +9,8 @@
 class Map {
  protected:
   //  matrix of 1's - walls, and 0's - paths
-  int** matrixOfWalls;
+  int mapSize = 42;
+  int** mapMatrix;
 
   int numWalls;
   NonMoveableEntity** allWalls;
@@ -17,16 +18,22 @@ class Map {
   int id;
   std::string name;
 
+  // methods.
+  void setNumWalls(*int[42][42] mapMatrix);
+
  public:
   // should call generateMap and setWallPositions.
   Map();
+  ~Map();
 
   // generate matrix of 1's - walls, and 0's - paths
   int[42][42] generateMap();
 
-  //  set allWalls  with positions based on matrix of walls
-  void setWalls(*int[42][42] _matrixOfWalls);
-  NonMoveableEntity** getWalls();
+  //  set allWalls  with positions based on mapMatrix
+  void setWalls(*int[42][42] mapMatrix);
+  NonMoveableEntity** getWalls(int numWalls);
+
+  void mapDisplay();
 };
 
 #endif  // MAP_H
