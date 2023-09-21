1b46be73188571fa5bbcc4a8ad2a93cf524d0040
Benjamin-Bailes <139053033+Benjamin-Bailes@users.noreply.github.com>
2023-09-21 12:26:03 +0930

    misc

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,7 +1,8 @@
 #include "Enemy.h"
 
+#include "MoveableEntity.h"
+
 Enemy::Enemy() : MoveableEntity() {}
 
 Enemy::Enemy(int _xPos, int _yPos, int damage, int health, int size)
     : MoveableEntity(_xPos, _yPos, damage, health, size) {}
-
