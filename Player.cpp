0aa65b299d1028321bc6e6a328f21437d0118c4b
Kosta Kapetanos <kostakapetanos@student-10-201-09-096.wireless.adelaide.edu.au>
2023-09-21 12:29:28 +0930

    update

--- Player.cpp
+++ Player.cpp
@@ -18,7 +18,7 @@ void Player::performAction(char action) { // Function to handle player actions (
       // Check if moving up is valid (not into a wall, player or out of bounds
       // (if walls are not 1s) ) If valid, update player's position If there's
       // an enemy, attack it Otherwise, do nothing
-      if (yPos >= 0 && map[xPos][yPos + 1] != 1 && yPos + 1 != yPos_Enemy) {
+      if (map[xPos][yPos + 1] != 1 && yPos + 1 != yPos_Enemy) {
         yPos++;  // move player up (increase y position)
       } else if (yPos + 1 == yPos_Enemy) {
         attackOpponent();  // attack nearby enemy
@@ -32,7 +32,7 @@ void Player::performAction(char action) { // Function to handle player actions (
       // Check if moving down is valid (not into a wall, player or out of bounds
       // (if walls are not 1s) ) If valid, update player's position If there's
       // an enemy, attack it Otherwise, do nothing
-      if (yPos > 0 && map[xPos][yPos - 1] != 1 && yPos - 1 != yPos_Enemy) {
+      if (map[xPos][yPos - 1] != 1 && yPos - 1 != yPos_Enemy) {
         yPos--;  // move player down (decrease y position)
       } else if (yPos - 1 == yPos_Enemy) {
         attackOpponent();  // attack nearby enemy
@@ -46,7 +46,7 @@ void Player::performAction(char action) { // Function to handle player actions (
       // Check if moving left is valid (not into a wall, player or out of bounds
       // (if walls are not 1s) ) If valid, update player's position If there's
       // an enemy, attack it Otherwise, do nothing
-      if (xPos > 0 && map[xPos - 1][yPos] != 1 && xPos - 1 != xPos_Enemy) {
+      if (map[xPos - 1][yPos] != 1 && xPos - 1 != xPos_Enemy) {
         xPos--;  // move player left (decrease x position)
       } else if (xPos - 1 == xPos_Enemy) {
         attackOpponent();  // attack nearby enemy
@@ -60,7 +60,7 @@ void Player::performAction(char action) { // Function to handle player actions (
       // Check if moving right is valid (not into a wall, player or out of
       // bounds (if walls are not 1s) ) If valid, update player's position If
       // there's an enemy, attack it Otherwise, do nothing
-      if (xPos > 0 && map[xPos + 1][yPos] != 1 && xPos + 1 != xPos_Enemy) {
+      if (map[xPos + 1][yPos] != 1 && xPos + 1 != xPos_Enemy) {
         xPos++;  // move player right (increase x position)
       } else if (xPos + 1 == xPos_Enemy) {
         attackOpponent();  // attack nearby enemy
